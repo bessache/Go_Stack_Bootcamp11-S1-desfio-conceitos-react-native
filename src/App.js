29f3a04ec93702931b6c82b8ed901a3d8f81895e
Renato Bessa <bessache@outlook.com>
2020-04-17 11:38:19 -0300

    commit incial desafio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -11,8 +12,27 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories]= useState([]);
+  useEffect (()=> {
+    api.get('repositories').then(response =>{
+      setRepositories(response.data);
+    })
+  },[]);
+
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post (`repositories/${id}/like`);
+    
+    const likeRepository = response.data;
+
+    const repositoriesUpdated = repositories.map(repository=>{
+      if (repository.id === id) {
+        return likeRepository;
+      } else {
+        return repository;
+      }
+    });
+    setRepositories(repositoriesUpdated);
   }
 
   return (
@@ -34,18 +54,16 @@ export default function App() {
           <View style={styles.likesContainer}>
             <Text
               style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID={`repository-likes-${repository.id}`}
             >
-              3 curtidas
+              {repository.likes} curtida{repository.likes > 1 ? 's' : ''}
             </Text>
           </View>
 
           <TouchableOpacity
             style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            onPress={() => handleLikeRepository(repository.id)}
+            testID={`like-button-${repository.id}`}
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
